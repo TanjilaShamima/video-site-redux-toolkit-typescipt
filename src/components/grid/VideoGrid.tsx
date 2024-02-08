592af1939c28549e54d50d7be9632b13091d0bf6
Tanjila Akter Shamima <tanjila.akter@bjitgroup.com>
2024-02-08 13:41:39 +0600

    6.6 done

--- src/components/grid/VideoGrid.tsx
+++ src/components/grid/VideoGrid.tsx
@@ -1,15 +1,38 @@
+"use client";
+
 import VideoGridItem from "./VideoGridItem";
+import { ReduxStateType, useDispatch, useSelector } from "@/src/lib/providers";
+import { useEffect } from "react";
+import { fetchVideos } from "@/src/lib/redux/slices/VideoSlice/VideoSliceThunk";
+import { videoSlice } from "@/src/lib/redux/slices/VideoSlice/VideoSlice";
+import Loader from "../common/Loader";
 
 export default function VideGrid() {
-    return (
-        <section className="pt-12">
-            <section className="pt-12">
-                <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
-                    <VideoGridItem />
+  const { videos, isError, isLoadingLing, error } = useSelector(
+    (state) => state.videos
+  );
+  const dispatch = useDispatch();
 
-                    {/* <div className="col-span-12">some error happened</div> */}
-                </div>
-            </section>
-        </section>
-    );
+  useEffect(() => {
+    dispatch(fetchVideos());
+  }, []);
+
+  console.log("videos", videos);
+  return (
+    <section className="pt-12">
+      <section className="pt-12">
+        <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
+          {videos?.length > 0 ? (
+            videos.map((video) => (
+              <VideoGridItem key={video.id} video={video} />
+            ))
+          ) : (
+            <Loader />
+          )}
+
+          {/* <div className="col-span-12">some error happened</div> */}
+        </div>
+      </section>
+    </section>
+  );
 }
--- src/lib/redux/slices/VideoSlice/VideoSliceAPI.ts
+++ src/lib/redux/slices/VideoSlice/VideoSliceAPI.ts
@@ -3,6 +3,7 @@ import axiosInstance from "@/src/networks/NetworksAPI";
 export const getAllVideos = async() => {
     try {
         const response = await axiosInstance.get('/videos');
+        console.log("response", response);
         return response.data;
     } catch (error) {
         console.log(error);
